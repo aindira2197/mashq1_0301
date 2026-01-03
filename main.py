5189f5a829390720c533f40fefa5221d9a678661
Azadova Indira <aindira2197@gmail.com>
2026-01-03 20:44:35 +0500

    Add basic Python exercises for user input

---
+++ main.py
@@ -0,0 +1,20 @@
+#1-misol
+ism = input("Ism kirit: ")
+print(f"Salom,{ism}")
+
+#2-misol
+yosh = int(input("Yosh kirit: "))
+print(f"Yoshingiz {2026-yosh}")
+
+#3 - misol
+son = int(input("Son kirit: "))
+print(["Juft" if son%2 ==0 else "Toq"])
+
+#4 -misol
+son,son2,son3 = int(input('1-Son kirit: ')),int(input('2-Son kirit: ')),int(input('3-Son kirit: '))
+print(f"Eng kattasi : {max(son, son2, son3)}")
+
+#5 - misol
+c = int(input("C kirit: "))
+F = (c * 9 / 5) + 32
+print(F)
